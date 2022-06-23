047a99bea20dac524682a43aef4ac1012919a880
Sebastian <pazseba@hotmail.com>
2022-06-23 12:05:59 -0300

    Welcome class replacing Hello world message

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Welcome from './Welcome';
+
+const myFirstElement = <Welcome Name="Johnny"/>
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(myFirstElement);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
